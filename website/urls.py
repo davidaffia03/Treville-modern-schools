fde7b12e12d83be6a0f721402a2c72c86f4186e6
davidaffia03 <davidaffia03@gmail.com>
2026-09-25 13:30:51 +0100

    temp password reset

--- website/urls.py
+++ website/urls.py
@@ -2,9 +2,21 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+def temp_reset_password(request):
+    try:
+        user = User.objects.get(username="trevilleadmin")
+        user.set_password("jessytres")  # <-- change this to your new password
+        user.save()
+        return HttpResponse("Password changed successfully.")
+    except User.DoesNotExist:
+        return HttpResponse("User not found.")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("temp-reset-x92k4/", temp_reset_password),  # <-- our secret temporary URL
     path("", include("core.urls")),
 
 ]
